e154b2ae72d599a3568079d0d0cf1edd917f6655
Drakefly <csenat15@outlook.com>
2016-12-04 17:42:42 +0100

    Version Pre alpha Fais vite fait durant la pause :')

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+      <title>Ma page</title>
+      <meta charset="utf-8">
+	  <link rel="stylesheet"  href="style.css" />
+	  
+    </head>
+    
+	<body>
+	<h1>Bonjour et bienvenue sur le site de la clinique </h1>
+	<div>
+		<fieldset>
+			<form method="post" action="Login">
+			<p>
+	   			<label for ="Nom"> Nom: </label>
+	   			<input type="text" name="pseudo" id="name" />
+	   		</p>
+	   		<p>
+	   			<label for ="mdp"> Nom: </label>
+	   			<input type="password" name="MDP" id="motdepasse" />
+	   		</p>
+	   		<p>
+	   			<input type ="submit" value="Connexion" name="login" />
+	   			<input type ="reset" value="ToutEffacer" name ="f1" />
+			 </form>
+		</fieldset>
+
+		<?php 
+	   require_once ('connect.php');
+
+	  ?>
+
+
+	</body>
+
+</html>
