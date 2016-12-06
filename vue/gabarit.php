2883aaaeab3f43b03eed4c0d280b894842c5ef97
Drakefly <csenat15@outlook.com>
2016-12-06 17:52:04 +0100

    La page principale a avance

--- vue/gabarit.php
+++ vue/gabarit.php
@@ -14,20 +14,46 @@
 			<form method="post" action="#">
 			<p>
 	   			<label for ="Nom"> Nom: </label>
-	   			<input type="text" name="pseudo" id="pseudo" />
+	   			<input type="text" name="pseudo" id="pseudo" required />
 	   		</p>
 	   		<p>
 	   			<label for ="mdp"> Nom: </label>
-	   			<input type="password" name="motdepasse" id="motdepasse" />
+	   			<input type="password" name="motdepasse" id="motdepasse" required />
 	   		</p>
 	   		<p>
 	   			<input type ="submit" value="Connexion" name="submit" />
-	   			<input type ="reset" value="ToutEffacer" name ="f1" />
+	   			<input type ="reset" value="ToutEffacer" name ="f1"  />
 			 </form>
 		</fieldset>
 
 		<?php
-			echo $contenu; // ? 
+			require_once('controleur/controleur.php');
+			if (isset($_POST['submit'])){
+				$pseudo = $_POST['pseudo'];
+				$password = $_POST['password'];
+				$bonmotdepasse = CtlLogin($pseudo,$password); //Appel du controleur qui verifie si le mot de passe est correct Pas encore fait
+				if($bonmotdepasse=false){
+					//Afficher Mot De pass incorect en html pas en php
+					"<p name='erreur'> Mot de passe incorect</p>"
+				}else{
+					$type=recupererTypeEmploye($user,$pass);
+					switch ($type){
+						case agent:
+
+						break;
+						case medecin:
+
+						break;
+						case directeur:
+
+						break;
+						default:
+
+						break;
+
+					}
+				}
+			}
 		?>
 
 
