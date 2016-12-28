526badedd66ce8a198df747d27c5bdbf49d0519a
Drakefly <csenat15@outlook.com>
2016-12-28 17:29:00 +0100

    HTML non tésté pour medecin

--- vue/pageMedecin.php
+++ vue/pageMedecin.php
@@ -12,8 +12,64 @@
 	<?php
 		echo 'Connecté en tant que '.$_POST['pseudo'].'.';
 	?>
-	<div>
-	</div>
+	
+	<form name="choix" id="monForm5" action="#" method="post">
+	   	<fieldset>
+	   		<legend>Afficher un planing </legend>
+
+	   		 <p>
+       			Veuillez choisir par quel méthode afficher un planing<br />
+		    <input type="radio" name="type" value="nom" id="nom" /> 
+		    <label for="nom">Recherche par nom</label>
+		    <br />
+
+		    <input type="radio" name="type" value="date" id="date" /> 
+		    <label for="date">Recherche par date</label>
+       		<br />
+       		<p>
+	   			<input type ="choix" value="choix" name="boutonChoix" />
+	   		</p>
+	   		<!-- TODO 
+
+
+	   		ou ca:
+			<form name="afficherPlaning" id="monForm6" action="#" method="post">
+	   			<fieldset>
+	   			<legend>Afficher un planing via nom </legend>
+
+			   		<p>
+			   			<label for="name">Nom du medecin a afficher : </label>
+		       			<input type="text" name="pseudo" id="pseudo" required />
+		       		</p>
+
+		       		<p>
+			   			<input type ="submit" value="Afficher" name="boutonAfficherEDTNom" />
+			   		</p>
+			   	</fieldset>
+			</form>
+
+	   		ou ca:
+			<form name="afficherPlaning" id="monForm7" action="#" method="post">
+	   			<fieldset>
+		   			<legend>Afficher un planing via date </legend>
+				   		<p>
+				   			<label for="date">Date a rechercher : </label>
+			       			<input type="date" name="Date" id="pseudo" required />
+			       		</p>
+			       		<p>
+				   			<input type ="submit" value="Afficher" name="boutonAfficherEDTDate" />
+				   		</p>
+				</fieldset>
+			</form>
+
+	   		--> 
+	   		<?php 
+			
+
+	   		//TODO
+	   		 ?>
+	   	</fieldset>
+	</form>
 
 	</body>
 
